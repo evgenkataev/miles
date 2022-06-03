1fa1712b1f015f60ec1f00e3361da20c14dc7799
evgenkataev <evgenkataev@gmail.com>
2022-06-03 21:46:21 +0500

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        int p = 2000;// переменная для затраченных рублей
-        int m = (p / 20); // переменная для расчета миль
+        int pay = 2000;// переменная для затраченных рублей
+        int miles = (p / 20); // переменная для расчета миль
         System.out.println(m); // вывод на экран
 
     }
