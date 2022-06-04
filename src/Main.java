17f6dfa9eec6b5a44bb24be8e23cda94dd1205c8
Евгений Катаев <evgenkataev@gmail.com>
2022-06-04 13:05:29 +0500

    variables changed

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         int pay = 2000;// переменная для затраченных рублей
-        int miles = (p / 20); // переменная для расчета миль
-        System.out.println(m); // вывод на экран
+        int miles = (pay / 20); // переменная для расчета миль
+        System.out.println(miles); // вывод на экран
 
     }
 
